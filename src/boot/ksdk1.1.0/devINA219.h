40ac300ceb3fbfc495f1799da57a370c6f3dabd5
Omar Tanner <pixzlert@gmail.com>
2024-02-29 16:31:22 +0000

    fix

--- src/boot/ksdk1.1.0/devINA219.h
+++ src/boot/ksdk1.1.0/devINA219.h
@@ -38,7 +38,7 @@
 
 void		initINA219(const uint8_t i2cAddress, uint16_t operatingVoltageMillivolts);
 WarpStatus	readSensorRegisterINA219(uint8_t deviceRegister, int numberOfBytes);
-WarpStatus	writeSensorRegisterINA219(uint8_t deviceRegister, uint8_t payloadBtye);
+WarpStatus writeSensorRegisterINA219(uint8_t deviceRegister, uint16_t payload);
 WarpStatus 	configureSensorINA219(uint8_t payloadF_SETUP, uint8_t payloadCTRL_REG1);
 void		printSensorDataINA219(bool hexModeFlag);
 uint8_t		appendSensorDataINA219(uint8_t* buf);
